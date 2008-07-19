762bade1f556b6ec3b25718832b12045abbfabf2
Pedro Visintin <pedro.visintin@gmail.com>
2008-07-19 20:22:17 -0300

    change the require to support any dir

--- lib/open_flash_chart_lazy.rb
+++ lib/open_flash_chart_lazy.rb
@@ -1,5 +1,5 @@
-require 'open_flash_chart_lazy/open_flash_chart_lazy'
-require 'open_flash_chart_lazy/open_flash_chart_lazy_helper'
+require File.join(File.dirname(__FILE__),'open_flash_chart_lazy/open_flash_chart_lazy')
+require File.join(File.dirname(__FILE__), 'open_flash_chart_lazy/open_flash_chart_lazy_helper')
 
 ActionView::Base.class_eval do
   include ActionView::Helpers::OpenFlashChartLazyHelper
